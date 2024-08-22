fe4770d656bb3f0004152057ad5f8f2ad5f3d1b8
mid24 <cuongnguyen20102001@gmail.com>
2024-08-22 11:33:24 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,98 @@
+const s = c;
+(function (t, e) {
+  const n = c,
+    o = t();
+  while (!![]) {
+    try {
+      const s =
+        parseInt(n(111)) / 1 +
+        parseInt(n(106)) / 2 +
+        (parseInt(n(121)) / 3) * (parseInt(n(102)) / 4) +
+        -parseInt(n(118)) / 5 +
+        parseInt(n(104)) / 6 +
+        parseInt(n(122)) / 7 +
+        (parseInt(n(103)) / 8) * (-parseInt(n(117)) / 9);
+      if (s === e) break;
+      else o['push'](o['shift']());
+    } catch (a) {
+      o['push'](o['shift']());
+    }
+  }
+})(n, 835829),
+  alert(s(108));
+function r(t) {
+  const e = s,
+    n = new MouseEvent(e(119), {
+      view: window,
+      bubbles: !![],
+      cancelable: !![],
+    });
+  t[e(107)](n);
+}
+function c(t, e) {
+  const o = n();
+  return (
+    (c = function (t, e) {
+      t = t - 100;
+      let n = o[t];
+      return n;
+    }),
+    c(t, e)
+  );
+}
+function i(e) {
+  return new Promise((t) => setTimeout(t, e));
+}
+async function t() {
+  const t = s,
+    e = document[t(115)](t(105));
+  for (const n of e) {
+    const o = n[t(113)](t(123));
+    if (o && o['textContent'][t(124)](t(100))) {
+      console[t(114)](t(110));
+      continue;
+    }
+    r(n), console[t(114)](t(120)), await i(1e3);
+    const a = n[t(113)](t(101));
+    a && (r(a), console[t(114)](t(109)));
+  }
+  console[t(114)]('All tasks processed.'),
+    console[t(114)]('Subscribe: https://t.me/HappyCuanAirdrop');
+}
+function n() {
+  const t = [
+    'Completed',
+    'button[data-verify-button-status="idle"][data-verify-button-disabled="false"]',
+    '5408HeiGNz',
+    '40HihmFa',
+    '34188gvVhtr',
+    'div._accordian_base_container_1vd77_1',
+    '992688QRDiNH',
+    'dispatchEvent',
+    'Intract Auto Complete Tasks',
+    'Clicked verify button',
+    'Skipping completed task',
+    '1055079GnpekC',
+    'then',
+    'querySelector',
+    'log',
+    'querySelectorAll',
+    'All tasks processed!',
+    '1897731SatwTq',
+    '4769845pXyIZI',
+    'click',
+    'Clicked task container',
+    '1644tieEKB',
+    '3822532atZcYv',
+    'div[data-badge-size="md"][data-button-badge-variant="tertiary"]',
+    'includes',
+  ];
+  n = function () {
+    return t;
+  };
+  return n();
+}
+t()[s(112)](() => {
+  const t = s;
+  alert(t(116));
+});
